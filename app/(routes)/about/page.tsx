59e3606fda1ae90a95c15f37ffd39d0b450e11d0
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-01-04 10:47:23 +0900

    add routes in same routes folder

---
+++ app/(routes)/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <main>
+      <h1>this is about page</h1>
+    </main>
+  );
+}
---
+++ app/(routes)/plan/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Plan() {
+  return (
+    <main>
+      <h1>this is create your plan page</h1>
+    </main>
+  );
+}
