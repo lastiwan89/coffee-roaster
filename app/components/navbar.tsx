3d506b6be1d78e2805a1391ea3c2661dacaf8f16
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-01-04 10:46:36 +0900

    complete navbar

---
+++ app/components/navbar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import Logo from "./logo";
+import Burger from "./burger";
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between py-8">
+      <div>
+        <Logo />
+      </div>
+      <div className="hidden md:flex md:flex-row md:gap-6">
+        <Link
+          className="body-nav text-grey-light hover:text-grey-dark cursor-pointer text-center font-sans font-bold uppercase"
+          href="/"
+        >
+          home
+        </Link>
+        <Link
+          className="body-nav text-grey-light hover:text-grey-dark cursor-pointer text-center font-sans font-bold uppercase"
+          href="/about"
+        >
+          about us
+        </Link>
+        <Link
+          className="body-nav text-grey-light hover:text-grey-dark cursor-pointer text-center font-sans font-bold uppercase"
+          href="/plan"
+        >
+          create your plan
+        </Link>
+      </div>
+      <Burger />
+    </nav>
+  );
+}
