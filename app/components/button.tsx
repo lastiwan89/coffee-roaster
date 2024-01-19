44e076b8ec2a90bd15a2fe0a7a614e78714eee20
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-01-20 08:25:35 +0900

    update package

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -6,7 +6,7 @@ export default function Button({
   text?: string;
 }) {
   return (
-    <button className="bg-cyan-dark hover:bg-cyan-light text-cream-light rounded-sm px-8 py-4 font-serif text-[18px] font-black leading-[25px]">
+    <button className="rounded-sm bg-cyan-dark px-8 py-4 font-serif text-[18px] font-black leading-[25px] text-cream-light hover:bg-cyan-light">
       {text}
     </button>
   );
