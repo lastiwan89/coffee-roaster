a8e0ee13ecf9c230bf04414bddcc9f610b340da5
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-01-28 21:44:48 +0900

    update font size more with line height in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,24 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["var(--font-barlow)"],
+      serif: ["var(--font-fraunces)"],
+    },
+    fontSize: {
+      150: ["150px", { lineHeight: "72px" }],
+      96: ["96px", { lineHeight: "72px" }],
+      72: ["72px", { lineHeight: "72px" }],
+      48: ["48px", { lineHeight: "48px" }],
+      40: ["40px", { lineHeight: "40px" }],
+      32: ["32px", { lineHeight: "36px" }],
+      28: ["28px", { lineHeight: "28px" }],
+      24: ["24px", { lineHeight: "32px" }],
+      16: ["16px", { lineHeight: "26px" }],
+      15: ["15px", { lineHeight: "25px" }],
+      12: ["12px", { lineHeight: "15px", letterSpacing: "0.923px" }],
+    },
     extend: {
-      fontFamily: {
-        sans: ["var(--font-barlow)"],
-        serif: ["var(--font-fraunces)"],
-      },
       colors: {
         cyan: {
           dark: "#0E8784",
@@ -22,6 +35,7 @@ const config: Config = {
           light: "#83888F",
         },
         cream: {
+          dark: "##F4F1EB",
           light: "#FEFCF7",
         },
         pale: {
